4c78cd5df150d271f8032449c13520e7181ec945
Finsi Ennes <serafin.ennes@nomasystems.com>
2022-01-03 11:14:41 +0100

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Serafín Ennes Moscoso on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
